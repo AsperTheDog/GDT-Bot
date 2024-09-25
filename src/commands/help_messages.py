19795b696a54d6ab223c0b0653cae14529cd0269
AsperTheDog <guillerman0000@gmail.com>
2024-09-25 03:27:31 +0200

    more fixes

--- src/commands/help_messages.py
+++ src/commands/help_messages.py
@@ -1,6 +1,6 @@
 import json
 
-from disnake import ApplicationCommandInteraction, Permissions, Embed, Attachment
+from disnake import ApplicationCommandInteraction, Permissions, Embed, Attachment, Color
 from disnake.ext.commands import Cog, slash_command, InteractionBot
 
 
@@ -11,13 +11,12 @@ class HelperMsgCog(Cog):
     @slash_command(name="sendembed", description="Send a custom embed message", default_member_permissions=Permissions(administrator=True))
     async def sendCustomEmbed(self, inter: ApplicationCommandInteraction, file: Attachment):
         await inter.response.defer()
-        if file.content_type != "application/json":
-            await inter.edit_original_response(content="The file must be a JSON file.")
         data = (await file.read()).decode('utf-8')
         try:
             data = json.loads(data)
         except json.JSONDecodeError:
-            await inter.edit_original_response(content="The file must be a valid JSON file.")
+            embed = Embed(title="Error", description="The file must be a valid JSON file.", color=Color.red())
+            await inter.edit_original_response(embed=embed)
             return
         embed = Embed.from_dict(data)
         await inter.channel.send(embed=embed)
